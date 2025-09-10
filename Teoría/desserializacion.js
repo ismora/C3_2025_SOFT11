f7d1a20d4346d30711e365d7166d5ed55aacc1a6
vmora <vmora@ucenfotec.ac.cr>
2025-09-09 18:59:20 -0600

    [new] Archivos base para el taller

---
+++ Teoría/desserializacion.js
@@ -0,0 +1,4 @@
+const jsonString = '{"nombre": "Isabel", "edad": 29}';
+const persona = JSON.parse(jsonString);
+console.log(persona.nombre); // "Isabel"
+
---
+++ Teoría/serializacion.js
@@ -0,0 +1,4 @@
+const persona = { nombre: "Isabel", edad: 29 };
+const jsonString = JSON.stringify(persona);
+console.log(jsonString);
+
