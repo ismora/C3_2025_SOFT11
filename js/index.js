04235063f8157dccc319ce54b200bed88c3c36c4
zsfabian <fgutierrezs@ucenfotec.ac.cr>
2025-09-11 20:02:25 -0600

    [New] Clase 11/09

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 /*
 Pendiente
 */
+
+let personas;
+let divPersonas = document.getElementById("divPersonas")
+
+//Funcion para cargar JSON
+async function cargarJSON() {
+    const respuesta = await fetch("js/personas.json");
+    personas = await respuesta.json();
+    console.log(personas);
+    
+    for(let i = 0; i < personas.length; i++){
+    divPersonas.innerText += personas [i].nombre + "\n";
+        // InnerText permite definir el texto que se muestra en el div
+    }
+}
+
+// Llamada de la funcion
+cargarJSON();
