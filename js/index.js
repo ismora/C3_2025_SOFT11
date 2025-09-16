78710638b1b32cb6b0f3b40fd4b6fcbe35469e47
JAIME ESNIDER MORA GONZALEZ <jmoragon@ucenfotec.ac.cr>
2025-09-16 17:12:47 -0600

    [New]livepage with Json file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 /*
 Pendiente
 */
+let personas;
+let divPersonas = document.getElementById("divPersonas");
+
+// Función para cargar el JSON
+
+async function cargarJSON() {
+    const respuesta = await fetch("js/personas.json");
+    personas = await respuesta.json();
+    console.log(personas);
+
+    for(let i = 0; i < personas.length; i++){
+        divPersonas.innerText += personas [i].nombre + "\n";
+        // Innertext permite definir el texto que se muestra en el div
+
+    }
+
+}
+// LLamada de la función
+cargarJSON();
