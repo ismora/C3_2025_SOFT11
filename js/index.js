d5a12d3cb6f59af9f3724267d2ccb4b4c8ce6254
Raquel Bueno <rbuenon@ucenfotec.ac.cr>
2025-09-16 19:05:51 -0600

    [Updated] index and js/index.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 /*
 Pendiente
 */
+
+let personas;
+let divPersonas = document.getElementById("divPersonas");
+
+// Función para cargar el JSON
+async function cargarJSON() {
+    const respuesta = await fetch("js/personas.json");
+    personas = await respuesta.json();
+    console.log(personas);
+
+    for (let i = 0; i < personas.length; i++) {
+        divPersonas.innerText += personas[i].nombre + "\n";
+        // innerText permite definir el texto que se muestra en el div
+    }
+
+}
+
+//Llamada de la función
+cargarJSON();
