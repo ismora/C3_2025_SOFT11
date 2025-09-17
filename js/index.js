1cc413f360faf4f74bea144107806adb50d05e9c
Maria Jose Canalias Sanabria <mcanaliass@ucenfotec.ac.cr>
2025-09-16 22:01:59 -0600

    [Incomplete] JSON

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 /*
 Pendiente
 */
+
+let personas;
+
+async function cargarJSON(){
+const respuesta = await fetch("js/personas.json")
+    personas = await respuesta.json();
+    console.log(personas);
+}
+
+let divPersonas = document.getElementById("divPersonas")
