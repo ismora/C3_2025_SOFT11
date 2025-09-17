b87169f56731dbab678fb18b9d0b34ac7996f0b1
Maria Jose Canalias Sanabria <mcanaliass@ucenfotec.ac.cr>
2025-09-16 22:13:08 -0600

    [Complete] JSON Taller

--- js/index.js
+++ js/index.js
@@ -3,11 +3,17 @@ Pendiente
 */
 
 let personas;
+let divPersonas = document.getElementById("divPersonas")
 
-async function cargarJSON(){
-const respuesta = await fetch("js/personas.json")
+async function cargarJSON() {
+    const respuesta = await fetch("js/personas.json");
     personas = await respuesta.json();
     console.log(personas);
+    
+    for(let i = 0; i < personas.length; i++){
+    divPersonas.innerText += personas [i].nombre + "\n";
+        // InnerText permite definir el texto que se muestra en el div
+    }
 }
 
-let divPersonas = document.getElementById("divPersonas")
+cargarJSON();
