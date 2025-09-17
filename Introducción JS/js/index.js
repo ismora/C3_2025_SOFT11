3edd5e93a6b0dc11244e78fa4a2b858dab108559
vmora <vmora@ucenfotec.ac.cr>
2025-09-16 18:48:55 -0600

    [new] Archivo js base

---
+++ Introducción JS/js/index.js
@@ -0,0 +1,57 @@
+// Variable: Espacio de memoria en la computadora, en donde un programa almacena un dato que puede o no cambiar durante la ejecución.  
+
+// Forma incorrecta de crear variables.
+
+
+// Forma adecuada de crear variables.
+
+
+//Constante: Valor que no cambia durante la ejecución de un programa. 
+
+
+// Función: Bloque de código reutilizable que realiza una tarea específica.
+
+
+/* Crear una función para calcular el IMC con la siguiente fórmula:
+IMC = peso / estatura^2
+Mostrar en la consola un mensaje que diga "El IMC es: --"
+Datos de prueba en kg       m       IMC
+                    80      1.7     27.7
+                    60      1.8     18.5
+*/
+
+
+/* Crear una función para calcular el área de un rectángulo
+Datos de prueba:    base    altura      área
+                    5       3           15
+                    4                   16 
+                    ""      2           Error
+                    0       2           Error
+*/
+
+
+/*
+Ejercicio 1: Crear una función para convertir temperaturas entre Celsius y Fahrenheit.
+
+Diseñar una función llamada convertirTemperatura que convierta una temperatura entre grados Celsius (°C) y Fahrenheit (°F) según la unidad solicitada. 
+
+Datos de prueba:    Temperatura     Escala      Resultado esperado
+                    25              F           77
+                    32              C           0
+                    "100"           C           Error
+                    0               a           Error
+*/
+
+
+/*
+Ejercicio 2: Crear una función para verificar si una palabra es un palíndromo
+
+Diseñar una función llamada esPalindromo que determine si una palabra se lee igual de izquierda a derecha que de derecha a izquierda.
+
+Datos de prueba:    Palabra         Resultado esperado 
+                    analina         es palíndromo
+                    reconocer       es palíndromo
+                    casa            no es palíndromo
+
+Nota: Puede usar split(), reverse(), join() o toLowerCase(), sin embargo, debe investigar su uso.
+*/
