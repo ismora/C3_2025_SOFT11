bd9d35951b38c44056ce4c44b28c18e5568a5be6
vmora <vmora@ucenfotec.ac.cr>
2025-11-06 17:34:37 -0600

    [new] Vistas para el CRUD de usuarios

---
+++ front-end/js/usuario-listar.js
@@ -0,0 +1,28 @@
+const tablaUsuarios = document.getElementById("tblUsuarios").querySelector("tbody");
+
+async function cargarTabla() {
+    fetch("http://localhost:3000/usuarios", {
+        method: "GET",
+        headers: {
+            "Content-Type": "Application/json"
+        }
+    }).then(response => response.json()
+    ).then(listaUsuarios => {
+        tablaUsuarios.innerHTML = ""; // Limpiar la tabla
+        listaUsuarios.forEach(usuario => {
+            const fila = document.createElement("tr");
+            // `: Comilla francesa, permite utilizar variables o expresiones en un string. Por ejemplo dentro de de la fila crear la celda (td) con lo datos de usuario traidos de la BD (interpolación de variables: Insertar variables o expresiones directamente dentro de una cadena utilizando la sintaxis ${}) 
+            fila.innerHTML = `
+                <td> ${usuario.nombre} </td>
+                <td> ${usuario.correo} </td>
+                <td> ${usuario.cedula} </td>
+                <td> ${usuario.celular} </td>
+                `;
+            tablaUsuarios.appendChild(fila); // Agregar la fila creada en la tabla
+        })
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+cargarTabla();
